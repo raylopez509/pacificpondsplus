05148f537467cf741722a61b456b32f70154d584
raylopez509 <raylopez509@gmail.com>
2024-06-10 17:28:45 -0700

    Initial commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <Link href="/">Home</Link>
+      <Link href="/about">About</Link>
+    </nav>
+  );
+};
+
+export default Navbar;
