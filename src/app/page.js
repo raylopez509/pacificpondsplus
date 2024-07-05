a5ad8080e7e43898afbe5e4eefac0aa62d543c04
raylopez509 <raylopez509@gmail.com>
2024-07-05 13:41:27 -0700

    Setup pictures on grid

--- src/app/page.js
+++ src/app/page.js
@@ -4,20 +4,25 @@ export default function Home() {
   return (
     <main>
       <div>If you need help cleaning your pond, we're the ones to help!</div>
+
+      <div>
+        We service the following:
+        <li>Los Angeles Region</li>
+        <li>Orange County Region</li>
+      </div>
+
       <div>Here's some work we have done:</div>
-      <div className={styles.title}>
+
+      <div className={styles.imagecontainer}>
         <div>Before</div>
         <div>After</div>
+        <img className={styles.a1} src="pic1before.jpg"></img>
+        <img className={styles.a2} src="pic1after.jpg"></img>
+        <img className={styles.a3} src="pic2before.jpg"></img>
+        <img className={styles.a4} src="pic2after.jpg"></img>
       </div>
-      <div className={styles.images}>
-        <div className={styles.pic1}>
-          <img className={styles.pic1before} src="pic1before.jpg"></img>
-          <img className={styles.pic1after} src="pic1after.jpg"></img>
-        </div>
-        <div className={styles.pic2}>
-          <img className={styles.pic2before} src="pic2before.jpg"></img>
-          <img className={styles.pic2after} src="pic2after.jpg"></img>
-        </div>
+      <div>
+        If you're interested, please give us a call! We'll be happy to help!
       </div>
     </main>
   );
