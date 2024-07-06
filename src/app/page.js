28cf308fa476ec02d3417d2d08e1fb6ee52eb3f3
raylopez509 <raylopez509@gmail.com>
2024-07-05 17:44:13 -0700

    Home page content and margins updated.
    Added services and location lists

--- src/app/page.js
+++ src/app/page.js
@@ -3,16 +3,21 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <main>
-      <div>If you need help cleaning your pond, we're the ones to help!</div>
-
-      <div>
-        We service the following:
-        <li>Los Angeles Region</li>
-        <li>Orange County Region</li>
+      <div className={styles.textcontainer}>
+        <div>If you need help cleaning your pond, we're the ones to help!</div>
+        <div className={styles.servicelist}>
+          We have the following services:
+          <li>Pool Maintenance</li>
+          <li>Fountain Maintenance</li>
+          <li>Koi Pond Maintenance</li>
+        </div>
+        <div className={styles.locationlist}>
+          We service the following:
+          <li>Los Angeles Region</li>
+          <li>Orange County Region</li>
+        </div>
+        <div>Here's some work we have done:</div>
       </div>
-
-      <div>Here's some work we have done:</div>
-
       <div className={styles.imagecontainer}>
         <div>Before</div>
         <div>After</div>
@@ -21,7 +26,7 @@ export default function Home() {
         <img src="pic2before.jpg"></img>
         <img src="pic2after.jpg"></img>
       </div>
-      <div>
+      <div className={styles.endingtextcontainer}>
         If you're interested, please give us a call! We'll be happy to help!
       </div>
     </main>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ const Navbar = () => {
   return (
     <nav className={styles.navbar}>
       <Link href="/">Home</Link>
-      <Link href="/about">About</Link>
+      <Link href="/contact">Contact Us</Link>
     </nav>
   );
 };
