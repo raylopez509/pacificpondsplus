30bb818c38e9c01e0b9bd6430c68da4e08d0cb11
raylopez509 <raylopez509@gmail.com>
2024-07-09 11:02:04 -0700

    Created telephone link on the telephone number.

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,11 +4,13 @@ const Header = () => {
   return (
     <>
       <header className={styles.header}>
-        <div className={styles.logocontainer}>
+        <section className={styles.logocontainer}>
           <img className={styles.logo} src="pacificpondsplus.png"></img>
-        </div>
+        </section>
         <div className={styles.title}>Pacific Ponds Plus</div>
-        <div className={styles.phone}>(657)217-0895</div>
+        <a className={styles.phone} href="tel:(657)217-0895">
+          (657)217-0895
+        </a>
       </header>
     </>
   );
