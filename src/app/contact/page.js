64d5950d86143008667c500dd0892a0c3ddc4a30
raylopez509 <raylopez509@gmail.com>
2024-07-15 15:00:24 -0700

    Contact Us page created

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -2,27 +2,38 @@ import styles from "./page.module.css";
 export default function AboutPage() {
   return (
     <>
-      <p>
-        With over 10 years of experience*, we specialize in aquatic features
-        such as pools, spas, fountains, and ponds. Regardless of size or
-        structure, we deliver routine maintenance as well as repairs.
+      <p className={styles.text}>
+        With over 10 years of experience, we specialize in aquatic features such
+        as pools, spas, fountains, and ponds. Regardless of size or structure,
+        we deliver routine maintenance as well as repairs.
       </p>
       <section className={styles.contact}>
         <img src="contactus.jpg" className={styles.image}></img>
         <section className={styles.socials}>
-          <a href="tel:(657)217-0895">(657)217-0895</a>
-          <div>
-            <a href="mailto:pacificpondsplus@gmail.com">
+          <p>
+            Phone:{" "}
+            <a className={styles.phone} href="tel:(657)217-0895">
+              (657)217-0895
+            </a>
+          </p>
+          <p>
+            Email:{" "}
+            <a
+              className={styles.email}
+              href="mailto:pacificpondsplus@gmail.com"
+            >
               PacificPondsPlus@gmail.com
             </a>
-          </div>
-          <h1>Follow us on Instagram!</h1>
-          <a href="https://www.instagram.com/pacificpondsplus/">
-            @PacificPondsPlus
-          </a>
+          </p>
+          <p>
+            Follow us on Instagram!{" "}
+            <a href="https://www.instagram.com/pacificpondsplus/">
+              @PacificPondsPlus
+            </a>
+          </p>
         </section>
       </section>
-      <p>*actual years of experience may be less than 10 years</p>
+      <h1 className={styles.text}>Call now for a free estimate!</h1>
     </>
   );
 }
