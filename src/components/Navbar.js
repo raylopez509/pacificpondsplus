d3247b5e4d305a4e43c475685e630de5559e3887
raylopez509 <raylopez509@gmail.com>
2024-07-30 11:13:32 -0700

    updated navlinks
    Navlinks now become spans if they are on the current page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,21 @@
+"use client";
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import styles from "./navbar.module.css";
 
 const Navbar = () => {
+  const pathName = usePathname();
   return (
     <nav className={styles.navbar}>
-      <Link href="/">Home</Link>
-      <Link href="/contact">Contact Us</Link>
+      {/* <Link href="/">Home</Link>
+      <Link href="/contact">Contact Us</Link> */}
+      {pathName === "/" ? <span>Home</span> : <Link href="/">Home</Link>}
+      {pathName === "/contact" ? (
+        <span>Contact Us</span>
+      ) : (
+        <Link href="/contact">Contact Us</Link>
+      )}
     </nav>
   );
 };
